ad297194098f6af9c0e2761059e662e6f8d16442
samuel355 <asamuel355@yahoo.com>
2024-10-08 14:14:20 +0000

    updates on displaying chat messages

--- src/components/chat-messages.tsx
+++ src/components/chat-messages.tsx
@@ -3,6 +3,7 @@ import { useChatFetcher } from "@/hooks/use-chat-fetcher";
 import { Channel, User, Workspace } from "@/types/app";
 import { FC } from "react";
 import DotAnimatedLoader from "./dot-animated-loader";
+import Typography from "./ui/typography";
 
 type ChatMessagesProps = {
   userData: User;
@@ -33,16 +34,33 @@ const ChatMessages: FC<ChatMessagesProps> = ({
 }) => {
   const queryKey =
     type === "Channel" ? `channel:${chatId}` : `direct_message:${chatId}`;
-  const {data, status, fetchNextPage, hasNextPage, isFetchingNextPage} = useChatFetcher({
-    queryKey,
-    apiUrl,
-    paramkey,
-    paramValue,
-    pageSize: 10,
-  });
-  
-  if (status === "pending") return <DotAnimatedLoader />
-  return <div>Messages t</div>;
+  const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useChatFetcher({
+      queryKey,
+      apiUrl,
+      paramkey,
+      paramValue,
+      pageSize: 10,
+    });
+
+  if (status === "pending") return <DotAnimatedLoader />;
+  if (status === "error") {
+    return <div>Error Occured</div>;
+  }
+
+  const renderMessages = () =>
+    data.pages.map((page) =>
+      page.data.map((message) => (
+        <div key={message.id} className="">
+          {message ? message.content : "No message yet"}
+        </div>
+      ))
+    );
+  return (
+    <div className="flex flex-1 flex-col py-4 overflow-y-auto">
+      <div className="flex flex-col-reverse mt-auto">{renderMessages()}</div>
+    </div>
+  );
 };
 
 export default ChatMessages;
